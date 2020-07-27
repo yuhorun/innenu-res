295a9b5aba2ef80eac1f10981851655585d234d3
Yaoyi <13110235070@163.com>
2020-07-27 22:02:18 +0800

    add two  encryption fles

---
+++ script/appidInfo.ts
@@ -0,0 +1,4 @@
+export default {
+  wx33acb831ee1831a5: "e438e6c097c3ade497bca94d46729f48",
+  wx9ce37d9662499df3: "79efdd00bec7309491d0a374d1f0ddbc",
+} as Record<string, string>;
---
+++ script/loginInfo.ts
@@ -0,0 +1,5 @@
+export default {
+  host: "v3.mp.innenu.com",
+  user: "mp-v3",
+  password: "RGAspEWKK2MESrxJ",
+};
